5cdb02d38ff8aa71c574b0501ecb3e1126bfd440
AkiraWin <frog7bar@gmail.com>
2023-12-01 11:11:35 +0900

    first commit

---
+++ cl/main.py
@@ -0,0 +1,8 @@
+
+
+def core():
+    print('spam ham eggs..')
+
+
+if __name__ == '__main__':
+    core()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    extras_require={
+        'dev': ['pytest', 'flake8', 'mypy'],
+    },
+)
