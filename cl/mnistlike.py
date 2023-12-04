84bbc193e45a17981890c3c484101928156eb0b5
AkiraWin <frog7bar@gmail.com>
2023-12-04 23:47:06 +0900

    change mnistlike

--- cl/mnistlike.py
+++ cl/mnistlike.py
@@ -6,7 +6,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import linear_model, svm
 
 def show(img):
-    cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+    # cv2.namedWindow('test', cv2.WINDOW_NORMAL)
     cv2.imshow('test', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -15,18 +15,18 @@ def show(img):
 def view():
     with open('cl/data.pickle', 'rb') as f:
         data = pickle.load(f)
-        print(data[0][0].shape, len(data[0]))  # (784,) 839
-        X, y = data
-        chars = [a.reshape((28, 28)) for a in X]
-        img = np.vstack((
-            np.hstack(chars[:40]),
-            np.hstack(chars[40:80])
-        ))
-        print(y[:40])
-        print(y[40:80])
-        show(img)
+        print(len(data[0]))  # (784,) 839
+        width = 30
+        for i in range(10):
+            X = [x for x, y in zip(*data) if y == i]
+            chars = [a.reshape((28, 28)) for a in X]
+            remain = len(chars) % width
+            chars = chars + [np.zeros((28, 28))] * (width - remain)
+            hstacks = [np.hstack(chars[i:i+width]) for i in range(0, len(chars), width)]
+            img = np.vstack(hstacks)
+            show(img)
 
-# view()
+view()
 
 def usemnist():
     import scipy.io as scio
@@ -36,28 +36,31 @@ def usemnist():
     # X = data['trainX'][:3000]  # X は 60000x400 行列
     # y = data['trainY'].ravel()[:3000]  # y は 60000 x 1 行列、ravel()を使って60000次元ベクトルに変換
 
-    x = data['testX']
-    print(x[0])
-    # y = data['testY'].ravel()
-    # clf = svm.SVC(gamma=0.001)
-    # clf.fit(x, y)
-    # print(clf.score(x, y))
+    x = data['testX'][:769]
+    y = data['testY'].ravel()[:769]
 
-usemnist()
+    clf = svm.SVC(gamma=0.001)
+    clf.fit(x, y)
+    print(clf.score(x, y))
+
+# usemnist()
+
+def fitfit():
+    with open('cl/data.pickle', 'rb') as f:
+        data = pickle.load(f)
+
+        data_ = [(x, y) for x, y in zip(*data) if not np.isnan(y)]
+        X, y = zip(*data_)
+        # print(len(x))
+        # X, y = data
+        # y = [e if not np.isnan(e) else -1 for e in y]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 
-# with open('cl/data.pickle', 'rb') as f:
-#     data = pickle.load(f)
-
-#     data_ = [(x, y) for x, y in zip(*data) if not np.isnan(y)]
-#     x, y = zip(*data_)
-#     print(x[0])
-
-#     x_train, x_test = train_test_split(x)
-#     y_train, y_test = train_test_split(y)
-
-#     clf = MLPClassifier(max_iter=10000)
-#     # clf = linear_model.LogisticRegression(penalty='l2', C=10.0) # モデルの定義
-#     # clf = svm.SVC(gamma=0.001)
-#     clf.fit(x_train, y_train)
-#     print(clf.score(x_train, y_train))
+        clf = MLPClassifier(max_iter=10000)
+        clf = linear_model.LogisticRegression(penalty='l2', C=10.0) # モデルの定義
+        # clf = svm.SVC(gamma=0.001)
+        clf.fit(X_train, y_train)
+        print(clf.score(X_train, y_train))
+        print(clf.score(X_test, y_test))
