02ff36f2c8375d46639873137509af409b7d977a
AkiraMac <frogcherry7@gmail.com>
2023-12-24 19:10:46 +0900

    arowf

--- cl/hall_sim.py
+++ cl/hall_sim.py
@@ -13,6 +13,23 @@ def timer():
     yield
     print('Elapsed:', time.time() - t)
 
+def filling_vertically(arr: np.ndarray, n: int) -> np.ndarray:
+    '''zeros配列を縦に埋めていく、whileは危険か?
+    '''
+    i = 0
+    while n:
+        vertical = arr[:, i]
+        zero_indices = np.where(vertical==0)[0]
+        indices_size = zero_indices.size
+        if indices_size > n:
+            choice_indices = np.random.choice(zero_indices, size=n, replace=False)
+            arr[choice_indices, i] = 1
+            n = 0
+        else:
+            arr[zero_indices, i] = 1
+            n = n - indices_size
+        i += 1
+    return arr
 
 def odd_even():
     dist = np.array([0.417, 0.243, 0.251, 0.046, 0.032, 0.011])  # 2022/1
@@ -32,29 +49,36 @@ def odd_(pk: np.ndarray, num: int, dt: Any, seed=42):
     
     _, days = calendar.monthrange(dt.year, dt.month)
     total = num * days
-    dist = list(map(round, total * pk))
-    print(dist)
-    max_idx = dist.index(max(dist))
-    diff = total - sum(dist)
-    dist[max_idx] = dist[max_idx] + diff  # 余りの台数を一番台数の多い設定で調整する
+    dst = list(map(round, total * pk))
 
-    dst = np.zeros((days, num), dtype=np.int64)
-    # 設定6
-    quotient, remainder = divmod(dist[5], 3)
-    for i in [0, 10, 20]:
-        dst[i][:quotient] = [6] * quotient
-    dst[24][:remainder] = [6] * remainder
-
-    zero_indices = np.where(dst == 0)[0]
-
-    # for i, (s1, s3) in enumerate(zip(s1_list, s3_list)):
-    #     # if num - (s1 + s2 + s6) >= 0:
-    #     x = [1] * s1 + [3] * s3  + [0] * (num - (s1 + s3))#+ [6] * s6 + [4] * (num - (s1 + s2 + s6))
-    #     rng2 = np.random.RandomState(seed=seed+i)
-    #     rng2.shuffle(x)
-    #     a = np.array(x)
-    #     dst[i] = a
+    max_idx = dst.index(max(dst))
+    diff = total - sum(dst)
+    dst[max_idx] = dst[max_idx] + diff  # 余りの台数を一番台数の多い設定で調整する
     print(dst)
+    arr = np.zeros((days, num), dtype=np.int64)
+    # 設定3
+    arr = filling_vertically(arr, dst[2])
+    arr[arr==1] = 3
+    # 設定5
+    arr = filling_vertically(arr, dst[4])
+    arr[arr==1] = 5
+    # 設定6: 特日
+    quotient, remainder = divmod(dst[5], 3)
+    for i in [0, 10, 20]:
+        horizontal = arr[i, :]
+        zero_indices = np.where(horizontal==0)[0]
+        horizontal[zero_indices[:quotient]] = 1
+    arr = filling_vertically(arr, remainder)
+    arr[arr==1] = 6
+    # 設定1
+    arr[arr==0] = 1
+    
+    def shuffle_row(row):
+        np.random.shuffle(row)
+        return row
+
+    shuffled_arr = np.apply_along_axis(shuffle_row, axis=1, arr=arr)
+    print(shuffled_arr)
 
 
 def distribute_settings_(pk: np.ndarray, num: int, dt: datetime, seed=42):
@@ -150,8 +174,34 @@ if __name__ == '__main__':
         # df = main(num=32, months=3, step=1000)
         # print(df)
         # print(df['saf'].sum() / df['out'].sum())
+
         pk_odd, pk_even = odd_even()
         dt = datetime(2022, 1, 1)
         dst = odd_(pk_odd, 16, dt)
 
+        # n = 23
+        # a = np.zeros((7, 7))
+        # a[:, 0] = [2,2,2,2,2,2,2]
+
+        # def filling_vertically(arr: np.ndarray, n: int) -> np.ndarray:
+        #     '''zeros配列を縦に埋めていく 
+        #     '''
+        #     i = 0
+        #     while n:
+        #         vertical = arr[:, i]
+        #         zero_indices = np.where(vertical==0)[0]
+        #         indices_size = zero_indices.size
+        #         if indices_size > n:
+        #             choice_indices = np.random.choice(zero_indices, size=n, replace=False)
+        #             arr[choice_indices, i] = 1
+        #             n = 0
+        #         else:
+        #             arr[zero_indices, i] = 1
+        #             n = n - indices_size
+        #         i += 1
+        #     return arr
+        
+
+        # res = foo(a, n)
+        # print(res)
 
