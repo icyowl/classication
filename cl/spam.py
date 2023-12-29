b380c57d133a1c86380679fb81f64f495780a45b
AkiraMac <frogcherry7@gmail.com>
2023-12-29 17:40:53 +0900

    micro micro

--- cl/spam.py
+++ cl/spam.py
@@ -1,64 +1,59 @@
-# 4.1.3 分類における最小二乗（p.182）
 import numpy as np
-import matplotlib.pyplot as plt
-# from pylab import *
-import sys
+from concurrent.futures import ThreadPoolExecutor
+from contextlib import contextmanager
+import time
 
-K = 2    # 2クラス分類
-N = 100  # データ数
+@contextmanager
+def timer():
+    t = time.time()
+    yield
+    print('Elapsed:', time.time() - t)
 
-def f(x1, W_t):
-    # 決定境界の直線の方程式
-    a = - ((W_t[0,1]-W_t[1,1]) / (W_t[0,2]-W_t[1,2]))
-    b = - (W_t[0,0]-W_t[1,0])/(W_t[0,2]-W_t[1,2])
-    return a * x1 + b
 
-if __name__ == "__main__":
-    # 訓練データを作成
-    cls1 = []
-    cls2 = []
-    
-    # データは正規分布に従って生成
-    mean1 = [-1, 2]  # クラス1の平均
-    mean2 = [1, -1]  # クラス2の平均
-    cov = [[1.0,0.8], [0.8,1.0]]  # 共分散行列（全クラス共通）
+def imjug(bet=3., cherry_deno=39.35):
+    big = 431.16, 422.81, 422.81, 417.43, 417.43, 407.06
+    cbig = 1129.93, 1129.93, 1129.93, 1092.27, 1092.27, 1092.27
+    rbig = 2184.53, 2184.53, 2184.53, 1820.44, 1820.44, 1820.44
+    reg = 642.51, 590.41, 474.90, 448.88, 364.09, 364.09
+    creg = 1394.38, 1236.53, 1092.27, 1057.03, 851.12, 851.12
+    bell =  (1092.27,) * 6
+    clown = (1092.27,) * 6
+    grape = 6.02, 6.02, 6.02, 6.02, 6.02, 5.78
+    cherry =  (32.28,) * 6
+    replay = (7.298,) * 6
 
-    # ノイズなしデータ
-    cls1.extend(np.random.multivariate_normal(mean1, cov, int(N/2)))
-    cls2.extend(np.random.multivariate_normal(mean2, cov, int(N/2)))
+    p = 1/np.array([big, cbig, rbig, reg, creg, bell, clown, grape, cherry, replay])
+    p *= np.array([1., 1., 1., 1., 1., 1092.27/5734, 1092.27/14167, 1., 32.28/cherry_deno, 1.]).reshape(-1, 1)
+    q = 1 - np.sum(p, axis=0).reshape(1, -1)
+    pk = np.concatenate([p, q]).T
 
-    # データ行列Xを作成
-    temp = np.vstack((cls1, cls2))
-    temp2 = np.ones((N, 1))  # バイアスw_0用に1を追加
-    X = np.hstack((temp2, temp))
-    
-    # ラベル行列T（1-of-K表記）を作成
-    T = []
-    for i in range(int(N/2)):
-        T.append(np.array([1, 0]))  # クラス1
-    for i in range(int(N/2)):
-        T.append(np.array([0, 1]))  # クラス2
-    T = np.array(T)
-    
-    # パラメータ行列Wを最小二乗法で計算（式4.16）
-    X_t = np.transpose(X)
-    temp = np.linalg.inv(np.dot(X_t, X))  # 行列の積はnp.dot(A, B)
-    W = np.dot(np.dot(temp, X_t), T)
-    W_t = np.transpose(W)
-    print(W_t)
-    
-    # 訓練データを描画
-    x1, x2 = np.transpose(np.array(cls1))
-    plt.plot(x1, x2, 'rx')
-    
-    x1, x2 = np.transpose(np.array(cls2))
-    plt.plot(x1, x2, 'bo')
-    
-    # 識別境界を描画
-    x1 = np.linspace(-4, 8, 1000)
-    x2 = [f(x, W_t) for x in x1]
-    plt.plot(x1, x2, 'g-')
-    
-    plt.xlim(-4, 8)
-    plt.ylim(-8, 4)
-    plt.show()
+    out = np.array([3+42+bet, 3+42+bet, 3+42+bet, 3+16+bet, 3+16+bet, 3., 3., 3., 3., 3., 3.])
+    saf = np.array([294., 294+2., 294+1., 112., 112+2., 14., 10., 8., 2., 3., 0.])
+    saf += np.array([0., 2*14/21-2, 14/21-1, 2*14/21-2, 0., 2*14/21-2, 0., 0., 0., 0., 0.])
+
+    return pk, out, saf
+
+pk, out, saf = imjug()
+
+def func(i, size):
+    pk_ = pk[i-1]
+    np.random.seed(seed=42)
+    uniform_samples = np.random.uniform(0, 1, size=size)
+    cdf = pk_.cumsum()
+    cdf /= cdf[-1]
+    samples = np.searchsorted(cdf, uniform_samples)
+    hist, _ = np.histogram(samples, bins=pk_.size)
+    return i, np.sum(saf*hist) / np.sum(out*hist) * 100
+
+def run(size):
+    executor = ThreadPoolExecutor(max_workers=6)
+    futures = []
+    for i in range(1, 7):
+        future = executor.submit(func, i, size)
+        futures.append(future)
+
+    for f in futures:
+        print(*f.result())
+
+with timer():
+    run(size=8000*10000)
